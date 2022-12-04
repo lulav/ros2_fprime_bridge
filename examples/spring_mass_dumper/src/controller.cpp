9b14e937ca173505771a972808c30bd433cd0eba
Iftach Naftaly <58637596+iftahnaf@users.noreply.github.com>
2022-12-04 08:33:24 +0200

    using v instead of r derivative (#7)
    * using v instead of r derivarive
    
    * code cleanup

--- examples/spring_mass_dumper/src/controller.cpp
+++ examples/spring_mass_dumper/src/controller.cpp
@@ -7,7 +7,6 @@ Controller::Controller()
     _dt = TIME_STEP;
     
     _last_error = 0.0;
-    _last_r_feedback = 0.0;
 }
 
 void Controller::set_parameters(float kp, float ki, float kd, float r_req)
@@ -28,7 +27,7 @@ void Controller::step()
 {
     _error  = _params.r_req - _r;
     _i_error = _last_error + _params.ki * _error * _dt;
-    _d_error = -(_r - _last_r_feedback) / _dt;
+    _d_error = -_v;
 
     _p_element = _params.kp * _error;
     _i_element = _params.ki * _i_error;
@@ -37,7 +36,6 @@ void Controller::step()
     _u = _p_element + _i_element + _d_element;
 
     _last_error = _error;
-    _last_r_feedback = _r;
 }
 
 void Controller::get_outputs(control_signal &control_signal)
